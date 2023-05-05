9528d4e11259aef35f6eda65cd57a24e9fcb1e7a
N S <kwb020312@naver.com>
2023-05-05 22:52:45 +0900

    Feat-회사 세부정보

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -3,12 +3,36 @@ import { View, Text, Image } from "react-native";
 
 import styles from "./company.style";
 import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 // import { checkImageURL } from "../../../utils";
 
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
     <View style={styles.container}>
-      <Text>Company</Text>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
   );
 };
