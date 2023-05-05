74a9a72d68ab196105135490c8919119dff6cc84
N S <kwb020312@naver.com>
2023-05-05 23:30:10 +0900

    Feat-자격요건 및 상세설명 추가

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -36,6 +36,25 @@ const JobDetails = () => {
 
   const onRefresh = () => {};
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "자격사항":
+        return (
+          <Specifics
+            title={"자격사항"}
+            points={data[0].job_highlights?.Qualifications ?? ["해당사항 없음"]}
+          />
+        );
+      case "상세정보":
+        return (
+          <JobAbout
+            info={data[0].job_description ?? "제공된 데이터가 없습니다."}
+          />
+        );
+      case "의무사항":
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -82,6 +101,7 @@ const JobDetails = () => {
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
               />
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
